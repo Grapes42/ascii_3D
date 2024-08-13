55039a7f24568f359832b2d8b40eac38c5e794cb
Grapes42 <f.inggrapes@gmail.com>
2024-08-13 18:16:02 +1200

    spinning now works in all directions, working on comments

--- data_types.py
+++ data_types.py
@@ -12,42 +12,59 @@ class Object3D():
         self.pairs = pairs
         self.char = char
 
-        self.axis_dict = {"y": 0, "x": 1, "z": 2}
-
     def __str__(self) -> str:
         return f"Array: {self.array}\nPairs: {self.pairs}\nChar: {self.char}"
     
     def move(self, axis, amount):
+        pass
+
+    def rotate(self, origin, rads, axis):
+        
+        axes = [Y, X, Z]
+
+        axes.remove(axis)
+
+        print(axes)
+
+        # Define rotation origin
+        a_o = origin[axes[0]]
+        b_o = origin[axes[1]]
+
         for point in self.array[0]:
-            point[self.axis_dict[axis]] += amount
+            # Initial points
+            a_i = point[axes[0]]
+            b_i = point[axes[1]]
 
-    def rotate(self, origin, rads):
-        x_o = origin[X]
-        z_o = 10
+            # Distances from origin to initial point
+            a_from_origin = abs(a_i - a_o)
+            b_from_origin = abs(b_i - b_o)
 
-        for point in self.array[0]:
-            x_i = point[X]
-            z_i = point[Z]
+            # Uses the Pythagorean theorum to find the radius of the rotation
+            radius = math.sqrt( a_from_origin**2 + b_from_origin**2 )
 
-            x_from_origin = abs(x_i - x_o)
-            z_from_origin = abs(z_i - z_o)
+            # Determine what quadrant of the circle the point is in
+            # and adjust the offset and trig accordingly
 
-            radius = math.sqrt((x_from_origin)**2 + z_from_origin**2)
+            # Both positive
+            if b_i > b_o and a_i > a_o:
+                angle_corrector = math.atan( (b_from_origin) / (a_from_origin) )
 
-            if z_i > z_o and x_i > x_o:
-                angle_corrector = math.atan( (z_from_origin) / (x_from_origin) )
+            # b positive, a negative
+            elif b_i > b_o and a_i < a_o:
+                angle_corrector = math.atan( (a_from_origin) / (b_from_origin) ) + (.5 * pi)
 
-            elif z_i > z_o and x_i < x_o:
-                angle_corrector = math.atan( (x_from_origin) / (z_from_origin) ) + (.5 * pi)
+            # Both negative
+            elif b_i < b_o and a_i < a_o:
+                angle_corrector = math.atan( (b_from_origin) / (a_from_origin) ) + (pi)
 
-            elif z_i < z_o and x_i < x_o:
-                angle_corrector = math.atan( (z_from_origin) / (x_from_origin) ) + (pi)
+            # b negative, a positive
+            elif b_i < b_o and a_i > a_o:
+                angle_corrector = math.atan( (a_from_origin) / (b_from_origin) ) + (1.5 * pi)
 
-            elif z_i < z_o and x_i > x_o:
-                angle_corrector = math.atan( (x_from_origin) / (z_from_origin) ) + (1.5 * pi)
+            # Calculate the point's rotated value
+            a_f = radius * math.cos(rads+angle_corrector) + a_o
+            b_f = radius * math.sin(rads+angle_corrector) + b_o
 
-            x_f = radius * math.cos(rads+angle_corrector) + x_o
-            z_f = radius * math.sin(rads+angle_corrector) + z_o
-
-            point[X] = x_f
-            point[Z] = z_f
+            # Update shape
+            point[axes[0]] = a_f
+            point[axes[1]] = b_f
--- screen.py
+++ screen.py
@@ -42,23 +42,27 @@ class Screen():
         
 
     def plot(self, y, x, char="#"):
+        # Round points to char array
         y = round(y*self.y_correction + self.origin_y)
         x = round(x + self.origin_x)
 
+        # Check if point is within the screen's Y boundary
         if (y < 0) or (y > self.height-1):
             in_y_boundary = False
         else:
             in_y_boundary = True
 
+        # Check if point is within the screen's X boundary
         if (x < 0) or (x > self.width-1):
             in_x_boundary = False
         else:
             in_x_boundary = True
 
+        # If point is in both boundaries, render it
         if in_y_boundary and in_x_boundary:
             self.array[y, x] = char
 
-    def line(self, coord0, coord1, step_size=1):
+    def line(self, coord0, coord1, step_size=1, char="#"):
         rise = coord0[X] - coord1[X]
         run = coord0[Y] - coord1[Y]
 
@@ -73,7 +77,7 @@ class Screen():
                 end_y, end_x = copy(coord0[Y]), copy(coord0[X])
 
             while pos_x <= end_x:
-                self.plot(y=pos_y, x=pos_x)
+                self.plot(y=pos_y, x=pos_x, char=char)
                 
                 pos_x += step_size
                 pos_y += gradient * step_size
@@ -87,9 +91,9 @@ class Screen():
                 end_y, end_x = copy(coord0[Y]), copy(coord0[X])
 
             while pos_y <= end_y:
-                self.plot(y=pos_y, x=pos_x)
+                self.plot(y=pos_y, x=pos_x, char=char)
                 pos_y += step_size
 
-    def construct_pairs(self, points, pairs, step_size=1):  
+    def construct_pairs(self, points, pairs, step_size=1, char="#"):  
         for pair in pairs:
-            self.line(points[pair[0], 0], points[pair[1], 0])
+            self.line(points[pair[0], 0], points[pair[1], 0], char=char, step_size=step_size)

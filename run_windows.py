ae040ce49247dcf289f0ccee40fbdf8093b80d8e
Grapes42 <f.inggrapes@gmail.com>
2024-08-22 13:30:40 +1200

    wrong way lol, im tired

--- run_windows.py
+++ run_windows.py
@@ -1,4 +1,4 @@
 import os
 
-os.system("./code/pip install -r requirements.txt")
-os.system("./code/python main.py")
+os.system("pip install -r ./code/requirements.txt")
+os.system("python ./code/main.py")

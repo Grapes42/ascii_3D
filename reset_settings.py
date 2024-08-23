b3b35a85a9a2be87079568ffac44fe6398b05dd0
Grapes42 <f.inggrapes@gmail.com>
2024-08-23 16:46:42 +1200

    added some pallets to try

--- reset_settings.py
+++ reset_settings.py
@@ -12,6 +12,26 @@ fov: 90
 fg_color: "#b6f2d8"
 bg_color: "#131e19"
 
+# Below are some nice pallets from lospec.com to try 
+# (just replace the color values above with the colors
+# of your choice)
+
+# Station Defender by Calvino Sinclair: 
+# fg_color: "#a692b0"
+# bg_color: "#17141c"
+
+# 1Bit Monitor Glow by Polyducks:
+# fg_color: "#f0f6f0"
+# bg_color: "#222323"
+
+# Pixel Ink by Polyducks:
+# fg_color: "#edf6d6" 
+# bg_color: "#3e232c"
+
+# Gato Roboto - Virtual Cat by PureAsbestos:
+# fg_color: "#cc0e13"
+# bg_color: "##2b0000"
+
 
 # Control Settings
 
@@ -20,5 +40,5 @@ movement_speed: 0.1
 
 bullet_speed: 1"""
 
-with open("settings.eth", "w") as f:
+with open("settings.txt", "w") as f:
     f.write(settings)
